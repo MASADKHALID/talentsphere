a4bc1d24b9757e246aeb7d033b83f93443afbff7
Muhammad Asad Khalid <asadkhalidking55@gmail.com>
2026-02-15 01:08:39 +0500

    deply commit

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ const notificationRoutes = require('./routes/notificationRoutes');
 const app = express();
 
 // Middleware
+//app.use(cors({
+//  origin: "https://talentsphere-sigma.vercel.app"
+//}));
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
